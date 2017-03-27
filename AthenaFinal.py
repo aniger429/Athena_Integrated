f7edf359035b3413473adb3876e2d2fedcf49332
aniger429 <aniger429@gmail.com>
2017-03-28 00:55:08 +0800

    Athena Integrated
    All analysis displays can now be executed

--- AthenaFinal.py
+++ AthenaFinal.py
@@ -1,5 +1,3 @@
-import time
-
 from flask import Flask, request, render_template
 from flask import session
 
@@ -80,6 +78,10 @@ def view_candidate_data():
 
     if datasource in ["positive", "neutral", "negative"]:
         tweets = load_obj("Sentiment")[datasource]
+    elif datasource == "topic":
+        key = request.args.get('key')
+        tweets = load_obj("Topics")[key]['topic_tweets']
+        print(tweets)
 
     identify_candidate(tweets, cname=cname)
     data = load_obj("Candidate")
@@ -91,22 +93,60 @@ def view_candidate_data():
 
 @app.route('/topic')
 def view_topic_analysis():
-    topic_tweets = get_tweets_with_id()
-    tweets = [remove_usernames(t['tweet']) for t in topic_tweets]
-    final_list = tfidf_vectorizer(tweets, 1, 3)
-    lda = topic_lda_tfidf(tweets, 1, 1, 10, 100)
-    find_topic_tweets(lda,topic_tweets)
+    tweets = get_all_tweets()
+    datasource=request.args.get('datasource')
+    topic_for = "All Tweets"
 
+    if datasource == "candidate":
+        cname = request.args.get('candidate_name')
+        # tweets = [d for d in load_obj("Candidate") if d['cand_ana'][cname] != '-1']
+        tweets = load_obj("Tweets")
+        topic_for = "Candidate: "+cname
+    elif datasource == "sentiment":
+        senti = request.args.get('senti')
+        data = load_obj("Sentiment")
+        topic_tweets = data[senti]
+        topic_for=senti+"Sentiment"
+
+    tweets_only = [remove_usernames(t['tweet']) for t in tweets]
+    final_list = tfidf_vectorizer(tweets_only, 1, 3)
+    lda = topic_lda_tfidf(tweets_only, 1, 1, 10, 100)
+    find_topic_tweets(lda,tweets)
 
     return render_template("analysis/Topic/view_topic_analysis.html", tf_idf=final_list, topics_dict=lda,
-                           topic_analysis_for="ALL")
+                           topic_analysis_for=topic_for)
+
+@app.route('/topic_tweets')
+def view_tweets_for_topic():
+    key = request.args.get('key')
+    data = load_obj("Topics")
+    candidates_mentioned = identify_candidate_mentioned(data[key]['topic_tweets'])
+    return render_template("analysis/Topic/view_topic_tweets.html", key=key, topic=data[key], candidates_mentioned=candidates_mentioned)
+
 
 @app.route('/sentiment')
 def view_sentiment_analysis():
     tweets = get_all_tweets()
+    datasource = request.args.get('datasource')
+    sentiment_for = "All"
+
+    if datasource == "Topic":
+        key = request.args.get('key')
+        tweets = load_obj("Topics")[key]['topic_tweets']
+        sentiment_for = "Topic # "+key
+
+    elif datasource == "Candidate":
+        candidate_name = request.args.get('candidate_name')
+        tweets = load_obj("Tweets")
+        sentiment_for="Candidate: "+candidate_name
+
     positive_tweets, neutral_tweets, negative_tweets = compute_tweets_sentiment(tweets)
 
-    return render_template("analysis/view_tweets_sentiment.html", tweet_list=[negative_tweets, positive_tweets,neutral_tweets], sentiment_labels=['negative', 'neutral', 'positive'], sentiment_analysis_for="All")
+    positive_tweets = {'tweets': positive_tweets, 'candidate_mentioned': identify_candidate_mentioned(positive_tweets)}
+    neutral_tweets = {'tweets': neutral_tweets, 'candidate_mentioned': identify_candidate_mentioned(neutral_tweets)}
+    negative_tweets = {'tweets': negative_tweets, 'candidate_mentioned': identify_candidate_mentioned(negative_tweets)}
+
+    return render_template("analysis/view_tweets_sentiment.html", tweet_list=[negative_tweets, positive_tweets,neutral_tweets], sentiment_labels=['negative', 'neutral', 'positive'], sentiment_analysis_for=sentiment_for)
 
 
 @app.route('/analysis')
@@ -177,55 +217,9 @@ def find_more_kb_names():
     return render_template("knowledgebase.html", kb_name_list=get_all_kb_names(), kbFileList=get_all_kbfile(), duplicate=False)
 
 
-# Route that will process the file upload
-@app.route('/topic_tweets')
-def view_tweets_for_topic():
-    key = request.args.get('key')
-    data = load_obj("Topics")
-    candidates_mentioned = identify_candidate_mentioned(data[key]['topic_tweets'])
-    return render_template("analysis/Topic/view_topic_tweets.html", key=key, topic=data[key], candidates_mentioned=candidates_mentioned)
-
-
-@app.route('/topic_sentiment')
-def view_sentiment_for_topic():
-    key = request.args.get('key')
-    data = load_obj("Topics")
-    positive_tweets, neutral_tweets, negative_tweets = compute_tweets_sentiment(data[key]['topic_tweets'])
-
-    positive_tweets = {'tweets': positive_tweets, 'candidate_mentioned': identify_candidate_mentioned(positive_tweets)}
-    neutral_tweets = {'tweets': neutral_tweets, 'candidate_mentioned': identify_candidate_mentioned(neutral_tweets)}
-    negative_tweets = {'tweets': negative_tweets, 'candidate_mentioned': identify_candidate_mentioned(negative_tweets)}
-
-    return render_template("analysis/view_tweets_sentiment.html", tweet_list=[negative_tweets, positive_tweets,neutral_tweets], sentiment_labels=['negative', 'neutral', 'positive'], sentiment_analysis_for="Topic # "+key)
-
-
-@app.route('/sentiment_topic')
-def view_topic_for_sentiment():
-    senti = request.args.get('senti')
-    data = load_obj("Sentiment")
-    topic_tweets = data[senti]
-
-    tweets = [remove_usernames(t['tweet']) for t in topic_tweets]
-
-    final_list = tfidf_vectorizer(tweets, 1, 3)
-    lda = topic_lda_tfidf(tweets, 1, 1, 10, 100)
-    find_topic_tweets(lda,topic_tweets)
-
-
-    return render_template("analysis/Topic/view_topic_analysis.html", tf_idf=final_list, topics_dict=lda,
-                           topic_analysis_for=senti+"Tweets")
-
-
 @app.route('/new_workspace', methods=['POST'])
 def new_workspace():
     return new_analysis.new_analysis()
-
-    # return redirect(url_for('view_candidate_data'))
-
-#
-# @app.template_filter('split_space')
-# def split_space_filter(s):
-#     return s.strip().split()
 
 
 if __name__ == '__main__':
--- controllers/analysis_controller/new_analysis.py
+++ controllers/analysis_controller/new_analysis.py
@@ -1,7 +1,6 @@
 from flask import request, redirect, url_for, render_template
 from controllers.Candidate_Analysis.Candidate_Identification_Final import *
 from controllers.Topic_Analysis.Topic_Analysis import *
-from controllers.Sentiment_Analysis.Sentiment_Identification import *
 from collections import Counter
 from controllers.Topic_Analysis.Find_Topic_Tweets import *
 
@@ -11,20 +10,6 @@ def candidate_analysis(tweets, candidate_name):
     data = load_obj("Candidate")
     candidate_name_count = Counter([tweet['tweet'][tweet['cand_ana'][candidate_name]] for tweet in data])
     return candidate_name_count, data
-
-
-def topic_analysis(tweets):
-    tweets_only = [remove_usernames(t['tweet']) for t in tweets]
-
-    final_list = tfidf_vectorizer(tweets_only,1,3)
-    lda = topic_lda_tfidf(tweets_only, 1, 1, 10, 100)
-
-    find_topic_tweets(lda, tweets)
-    return final_list, lda
-
-
-def sentiment_analysis(tweets):
-    return compute_tweets_sentiment(tweets)
 
 
 def new_analysis():
@@ -41,21 +26,21 @@ def new_analysis():
 
         if slevel == "topic":
             print("slevel: topic")
-            data = load_obj('Tweets')
-            final_list, lda = topic_analysis(tweets)
-
-            return render_template("analysis/Topic/view_topic_analysis.html", tf_idf=final_list, topics_dict=lda, topic_analysis_for=candidate_name)
+            # data = load_obj('Tweets')
+            # final_list, lda = topic_analysis(tweets)
+            return redirect(url_for('view_topic_analysis', datasource='candidate', candidate_name=candidate_name))
+            # return render_template("analysis/Topic/view_topic_analysis.html", tf_idf=final_list, topics_dict=lda, topic_analysis_for=candidate_name)
 
         elif slevel == "sentiment":
             print("slevel: sentiment")
-            tweets = load_obj('Tweets')
-            print(tweets)
-            # tweets = [{'tweet': d['tweet'], '_id':d['_id']} for d in data]
-            positive_tweets, neutral_tweets, negative_tweets = sentiment_analysis(tweets)
+            # tweets = load_obj('Tweets')
+            # positive_tweets, neutral_tweets, negative_tweets = sentiment_analysis(tweets)
+            #
+            # return render_template("analysis/view_tweets_sentiment.html",
+            #                        tweet_list=[negative_tweets, positive_tweets, neutral_tweets],
+            #                        sentiment_labels=['negative', 'neutral', 'positive'], sentiment_analysis_for=candidate_name)
+            return redirect(url_for('view_sentiment_analysis', datasource='Candidate', candidate_name=candidate_name))
 
-            return render_template("analysis/view_tweets_sentiment.html",
-                                   tweet_list=[negative_tweets, positive_tweets, neutral_tweets],
-                                   sentiment_labels=['negative', 'neutral', 'positive'], sentiment_analysis_for=candidate_name)
 
         return render_template("View Data/view_candidate_data.html", candidate_name_count=candidate_name_count,
                                candidate_data=get_specific_candidate_names(candidate_name),
@@ -63,42 +48,45 @@ def new_analysis():
     # first level
     elif (flevel == "topic"):
         print("flvel: topic")
-        topic_tweets = list(get_all_tweets())
-        final_list, lda = topic_analysis(topic_tweets)
+        # topic_tweets = list(get_all_tweets())
+        # final_list, lda = topic_analysis(topic_tweets)
 
-        if slevel == "none":
-            print("slevel: none")
-        elif slevel == "candidate":
-            print("slevel: candidate")
+        # if slevel == "none":
+        #     print("slevel: none")
+        # elif slevel == "candidate":
+        #     print("slevel: candidate")
+        #
+        #     if tlevel == "none":
+        #         print("tlevel: none")
+        #     elif tlevel == "sentiment":
+        #         print("tlevel: sentiment")
+        # else:
+        #     print("slevel: sentiment")
+        #     if tlevel == "none":
+        #         print("tlevel: none")
+        #     elif tlevel == "candidate":
+        #         print("tlevel: candidate")
 
-            if tlevel == "none":
-                print("tlevel: none")
-            elif tlevel == "sentiment":
-                print("tlevel: sentiment")
-        else:
-            print("slevel: sentiment")
-            if tlevel == "none":
-                print("tlevel: none")
-            elif tlevel == "candidate":
-                print("tlevel: candidate")
-
-        return render_template("analysis/Topic/view_topic_analysis.html", tf_idf=final_list, topics_dict=lda, topic_analysis_for="ALL")
+        return redirect(url_for('view_topic_analysis'))
     # first level
     else:
         print("flevel: sentiment")
 
-        if slevel == "none":
-            print("slevel: none")
-        elif slevel == "candidate":
-            print("slevel: candidate")
+        # if slevel == "none":
+        #     print("slevel: none")
+        # elif slevel == "candidate":
+        #     print("slevel: candidate")
+        #
+        #     if tlevel == "none":
+        #         print("tlevel: none")
+        #     elif tlevel == "topic":
+        #         print("tlevel: topic")
+        # else:
+        #     print("slevel: topic")
+        #     if tlevel == "none":
+        #         print("tlevel: none")
+        #     elif tlevel == "candidate":
+        #         print("tlevel: candidate")
 
-            if tlevel == "none":
-                print("tlevel: none")
-            elif tlevel == "topic":
-                print("tlevel: topic")
-        else:
-            print("slevel: topic")
-            if tlevel == "none":
-                print("tlevel: none")
-            elif tlevel == "candidate":
-                print("tlevel: candidate")
+
+        return redirect(url_for('view_sentiment_analysis'))
